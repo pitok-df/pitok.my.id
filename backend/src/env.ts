e539ff66f45a6de2bebc50513644a46fec137320
Pito Desri Pauzi <108637369+PitokDf@users.noreply.github.com>
2026-09-12 08:27:06 +0700

    retest

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -2,11 +2,11 @@ import { App, z } from "@buntok/core";
 
 export const env = App.validateEnv({
   PORT: z.coerce.number().default(1212),
-  JWT_SECRET: z.string(),
+  JWT_SECRET: z.string().optional(),
   AUTH_STORE: z.enum(["header", "cookie"]).default("header"),
   AUTH_COOKIE: z.string().default("session"),
   NODE_ENV: z
     .enum(["development", "production", "test"])
     .default("development"),
-  DATABASE_URL: z.url(),
+  DATABASE_URL: z.url().optional(),
 });
